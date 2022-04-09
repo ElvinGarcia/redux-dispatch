f42d2443399502ecd3e39c0eda5784780bcdb151
Elvin <Elvin@ElvinGarcia.com>
2022-04-09 18:31:18 -0400

    done

--- js/reducer.js
+++ js/reducer.js
@@ -7,7 +7,23 @@ function changeState(state, action){
   }
 }
 
-let state = {count: 0}
-let action = {type: 'INCREASE_COUNT'}
+  function dispatch(action) {
+    state = changeState(state, action)
+    render();
+    return state;
+  }
 
-changeState(state, action)
+  function render() {
+    document.body.textContent = state.count;
+  }
+
+
+let state = {count: 0}
+
+// displays the initial values
+render();
+
+
+
+
+
